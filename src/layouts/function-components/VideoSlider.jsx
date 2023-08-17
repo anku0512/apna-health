3ba7a56729da6dba1eabf86c6bdc636c9ed808fc
ankush <anku0512@gmail.com>
2023-08-17 18:20:52 +0530

    video slider

--- src/layouts/function-components/VideoSlider.jsx
+++ src/layouts/function-components/VideoSlider.jsx
@@ -1,29 +1,80 @@
-import ReactPlayer from 'react-player'
+// video slider
+
+import { useRef, useState } from "react";
+import { Star } from "react-feather";
+import SwiperCore, { Autoplay, Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 const VideoSlider = ({ list }) => {
+  SwiperCore.use([Pagination, Autoplay]);
+  const [swiper, setSwiper] = useState(null);
+  const paginationRef = useRef(null);
 
   return (
-    <div className="video-slider">
-      
-      {list.map((item, i) => (
-        <div key={i} className="video-card">
-          
-          <ReactPlayer 
-            url={item.video}
-            playsinline
+    <div className="video-carousel relative">
+      <Swiper
+        pagination={{
+          type: "bullets",
+          el: paginationRef.current,
+          clickable: true,
+          dynamicBullets: true,
+        }}
+        onSwiper={(swiper) => {
+          setSwiper(swiper);
+        }}
+        // loop={true}
+        modules={[Pagination, Autoplay]}
+        loop={true}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+          stopOnLastSlide: false
+        }}
+        slidesPerView={1}
+        breakpoints={{
+          992: {
+            slidesPerView: 2,
+          },
+          1200: {
+            slidesPerView: 3,
+          },
+        }}
+      >
+        {list.map((item, i) => (
+          <SwiperSlide key={"feature-" + i}>
+            
+            <div className="video-card">
+              
+            <video 
             controls
-            playing= {true}
-            loop
-            width='300px'
-            height='300px'
-          />
+            autoPlay
+            muted
+  type="video/mp4"
+  src={item.video}
+  alt={item.alternate}
+  loop
+  width="300"
+  height="300"
+/>
 
-          
-          <p className="text-center">{item.comment}</p>
+              
+              <p className="text-center">{item.comment}</p>
 
-        </div>
-      ))}
+            </div>
 
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <div className="relative flex justify-center">
+        <div
+          width="100%"
+          className="swiper-pagination reviews-carousel-pagination !bottom-0"
+          style={{ width: "100%" }}
+          ref={paginationRef}
+        ></div>
+      </div>
     </div>
   );
 };
