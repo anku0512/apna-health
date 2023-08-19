3a0f6a917a970f1597bd8f7f45a0428042db52b7
ankush <anku0512@gmail.com>
2023-08-19 13:20:20 +0530

    video testimonial restored

--- src/layouts/function-components/VideoSlider.jsx
+++ src/layouts/function-components/VideoSlider.jsx
@@ -1,71 +1,30 @@
-// video slider
-
-import { useRef, useState } from "react";
-import { Star } from "react-feather";
-import SwiperCore, { Autoplay, Pagination } from "swiper";
-import "swiper/css";
-import "swiper/css/pagination";
-import { Swiper, SwiperSlide } from "swiper/react";
-
-
+import ReactPlayer from 'react-player'
 
 const VideoSlider = ({ list }) => {
 
-  const [swiper, setSlider] = useState(null);
-
-  // Keep track of current playing video index
-  const [activeVideoIndex, setActiveVideoIndex] = useState(0);
-
-  // Ref to current playing video
-  const videoRef = useRef(null);
-
-  // useEffect(() => {
-  //   const swiper = swiperRef.current;
-    
-  //   if (swiper) {
-  //     swiper.slides.forEach((slide, index) => {
-  //       slide.on('transitionStart', () => {
-  //         setActiveVideoIndex(index);
-  //       });
-  //     });
-  //   }
-  // }, [swiper]);
-
-  // Stop current playing video when slide changes
-  const onSlideChange = () => {
-    if (videoRef.current) {
-      videoRef.current.pause();
-    }
-  };
-  
-
   return (
-    <Swiper
-      onSlideChange={onSlideChange} 
-      onSwiper={setSlider}
-      loop={true}
-    >
-      {list.map((item, index) => (
-        <SwiperSlide key={index}>
-          
-          <div className="video-card">
-          <video 
-            ref={index === activeVideoIndex ? videoRef : null}
+    <div className="video-slider">
+
+      {list.map((item, i) => (
+        <div key={i} className="video-card">
+
+          <ReactPlayer 
+            url={item.video}
+            playsinline
             controls
-            type="video/mp4"
-            src={item.video}
-            alt={item.alternate}
-            width="300"
-  height="300"
+            playing= {true}
+            width='300px'
+            height='300px'
           />
-        <p className="text-center">{item.comment}</p>
 
-</div>
-        </SwiperSlide>
+
+          <p className="text-center">{item.comment}</p>
+
+        </div>
       ))}
-    </Swiper>
-    
-  );
 
+    </div>
+  );
 };
+
 export default VideoSlider;
